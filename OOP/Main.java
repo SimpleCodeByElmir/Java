4677b6e8cda5cc444d1250ebe02b2af6cb609943
Elmir_Musaev <worldrap4@gmail.com>
2023-04-16 17:06:56 +0400

    working with java classes

--- OOP/Main.java
+++ OOP/Main.java
@@ -3,7 +3,12 @@ import my_package.*;
 public class Main {
   public static void main (String[] args) {
     Person alex = new Person();
-    alex.age = 22;
-    System.out.println("Hello, World!\n" + alex.age);
+	alex.name = "Alesandro";
+    alex.age = 15;
+	alex.height = 1.75;
+	alex.weight = 55.2;
+	alex.greetings();
+	alex.talking();
+	alex.goodbye();
   }
 }
--- OOP/my_package/Person.java
+++ OOP/my_package/Person.java
@@ -1,5 +1,22 @@
 package my_package;
 
 public class Person {
+
+  public String name;
   public int age;
+  public double height;
+  public double weight;
+  
+
+  public void greetings() {
+    System.out.printf("Hello, my name is %s.\n", name);
+  }
+
+  public void talking() {
+    System.out.println("I'am " + age + " years old. I like to ride a bike. And just so You to know my height is " + height + " and my weight is " + weight + " kilograms.");
+  }
+
+  public void goodbye() {
+    System.out.println("Nice to meet You! Goodbye!");
+  }
 }
