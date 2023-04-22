f763650f2fff1288b9c59fd1c3ad1ed333907798
Elmir_Musaev <worldrap4@gmail.com>
2023-04-22 23:29:26 +0400

    exceptions in java

---
+++ exceptions.java
@@ -0,0 +1,25 @@
+public class exceptions {
+  public static void main(String[] args) {
+
+    int my_array[] = new int[5];
+    
+    try {
+      //my_array[6] = 6;
+      //my_array[6] = Integer.parseInt("gfd");
+      my_array[0] = 11;
+      if (my_array[0] > 10) {
+        throw new Exception("Throwing exception: my_array[0] > 10.");
+      }
+    }
+    catch (ArrayIndexOutOfBoundsException ex) {
+      System.out.println("Array out of bounds.");
+    }
+    catch (NumberFormatException ex) {
+      System.out.println("Invalid convertion from string to int.");
+    }
+    catch (Exception ex) {
+      System.out.println(ex.getMessage());
+    }
+    System.out.println("Program ended correctly.");
+  }
+}
